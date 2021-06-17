8fdd9873a662e68d03be121ed3b4122223a38b58
Alan Miguel Rocha <alanmrochadeveloper@gmail.com>
2021-06-17 13:18:36 -0300

    finished2

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as config from 'config';
+import { Logger } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.setGlobalPrefix('api');
-  await app.listen(3000);
+  const logger = new Logger('bootstrap');
+  const serverConfig = config.get('server');
+  console.log(serverConfig);
+  const port = process.env.PORT || serverConfig.port;
+  console.log(port);
+  await app.listen(port);
+  logger.log(`Application listing to port = ${port}`);
 }
 bootstrap();
